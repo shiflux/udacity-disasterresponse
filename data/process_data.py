c23140ccf38ce0b4f5f00be152aaddfd8142f78d
artem <pt.artem@gmail.com>
2022-09-04 15:52:33 +0200

    Added replace when inserting existing values in db

--- data/process_data.py
+++ data/process_data.py
@@ -66,7 +66,7 @@ def save_data(df: pd.DataFrame, database_filename: str) -> None:
         database_filename (str): path to sqlite db
     '''
     engine = create_engine('sqlite:///{}'.format(database_filename))
-    df.to_sql('DisasterMessages', engine, index=False)  
+    df.to_sql('DisasterMessages', engine, index=False, if_exists='replace')  
 
 
 def main():
